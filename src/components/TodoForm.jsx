50386bd8c3964f57fdc849d798b26818e326dec7
Nathlo <thyalan.aurlence@gmail.com>
2024-08-28 12:24:23 -0400

    built the form and addTodo function

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react'
 
-const TodoForm = () => {
+const TodoForm = ( {addTodo} ) => {
   // initialise the iput to a empty string
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState('');
+
+  const handleSubmit = (e) => {
+      e.preventDefault(); // to prevent the page reloading
+
+      addTodo(value);
+
+      setValue("");
+  }
 
   return (
-    <form className='TodoForm'>
-        <input type='text' className='todo-input' placeholder='What is the task today?'
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' className='todo-input' value={value} placeholder='What is the task today?'
               onChange={ (e) => setValue(e.target.value) }  />
         <button type='submit' className='todo-btn'>Add Task</button>
     </form>
