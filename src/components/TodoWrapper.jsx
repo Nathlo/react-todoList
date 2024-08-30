b63f9e0762583ea8cf225f2404c665fce76bec29
Nathlo <thyalan.aurlence@gmail.com>
2024-08-30 12:48:26 -0400

    Marking Todos as Complete

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -16,12 +16,24 @@ const TodoWrapper = () => {
       console.log(todos);
   }
 
+  const toggleComplete = id => {
+    // first, mapping through the todo we are looking for with its ID we passed in
+    // if todo's ID similar, we take a copy of that todo (...todo)
+    // then, we update the 'completed' value. If it's false-->become true, and vice versa
+    // if if todo's ID different, so return ID
+    setTodos( todos.map(todo => todo.id === id ? {...
+      todo, completed: !todo.completed} : todo ) )
+  }
+
   return (
     <div className='TodoWrapper'>
+      <h1>Tâches Faites !</h1>
+
         <TodoForm addTodo={addTodo} />
 
         {todos.map( (todo, index)  => (
-            <Todo task={todo} key={index} />
+            <Todo task={todo} key={index} 
+              toggleComplete={toggleComplete}/>
         ))}
      
     </div>
